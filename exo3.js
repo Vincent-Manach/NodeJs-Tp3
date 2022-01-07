fa8b2f447963ae89eb81ceffd2bf4f38e48d9529
Vincent <31652038+Vincent-Manach@users.noreply.github.com>
2022-01-07 10:16:41 +0100

    Add exo3

---
+++ exo3.js
@@ -0,0 +1,36 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+});
+
+function DOB(date) {
+    try {
+        myPromise(date).then(res => console.log(res));
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+const myPromise = (date) => {
+    return new Promise((resolve, failure) => {
+        const dob = new Date (date)
+        const ageOfMaj = new Date('2004-01-01')
+        console.log(dob +'fdrfdr'+ ageOfMaj)
+        if (dob < ageOfMaj ) {
+            resolve('True')
+            return true
+        } else if (dob > ageOfMaj ) {
+            failure('Nope')
+        }
+    })
+}
+
+DOB("1997-08-24")
+
+server.listen(3007);
+
+
+// Créez une fonction qui prend en paramètre une date de naissance d’une personne au format DD/MM/YYYY. Si la 
+// personne est mineure, la promesse échoue, sinon elle renvoie true
+
